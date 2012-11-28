2bc8143b00de7272566ac4561eef7eb8af4bf472
Brandon Roth <brandon@rocketmade.com>
2012-11-27 19:39:14 -0700

    Create hashmap lookup table for n-grams

--- src/markov.scala
+++ src/markov.scala
@@ -2,10 +2,13 @@ import java.util.Scanner
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.MutableList
 import collection.mutable.HashMap
+
 object markov {
 	 
 	def main(args: Array[String]){
-	  val scanner = new Scanner(io.Source.fromFile("mice.txt").mkString.toLowerCase)
+	  val scanner = new Scanner(io.Source.fromFile("ibs.txt").mkString.toLowerCase)
+	  //val string = "Three blind mice Three blind cats Go to the store"
+	  //val scanner = new Scanner(string)
 	  val list = new ArrayBuffer[String]
 	  while(scanner.hasNext()){
 	    list += scanner.next
@@ -28,10 +31,22 @@ object markov {
 		nGrams += shingle
 	  }
 	  
+	  val map = new HashMap[MutableList[String],MutableList[String]]
 	  for (i <- nGrams){
-	    println(i.toString())
+		  val key = i.take(n-1)
+	      if (map.contains(key)){
+	        var tmpNGram = map.apply(key)
+	    	 tmpNGram = tmpNGram ++ i.drop(n-1)
+	    	 map.update(key, tmpNGram)
+	      }
+	      else{
+	          map.update(key, i.drop(n-1))
+	      }
 	  }
 	  
+	  for(i <- map){
+	    println(i)
+	  }
 	}	
 
 }
